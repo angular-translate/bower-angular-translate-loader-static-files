84a1f3278de701572195cb9b6f290e5fb8404aeb
Zertz-Lenovo <pier_taz@yahoo.com>
2014-12-10 13:49:53 -0500

    Add support for multiple files

--- angular-translate-loader-static-files.js
+++ angular-translate-loader-static-files.js
@@ -8,23 +8,68 @@ angular.module('pascalprecht.translate').factory('$translateStaticFilesLoader', 
   '$http',
   function ($q, $http) {
     return function (options) {
-      if (!options || (!angular.isString(options.prefix) || !angular.isString(options.suffix))) {
-        throw new Error('Couldn\'t load static files, no prefix or suffix specified!');
+      if (!options || (!angular.isArray(options.files) && (!angular.isString(options.prefix) || !angular.isString(options.suffix)))) {
+        throw new Error('Couldn\'t load static files: no files or prefix and suffix specified!');
       }
-      var deferred = $q.defer();
-      $http(angular.extend({
-        url: [
-          options.prefix,
-          options.key,
-          options.suffix
-        ].join(''),
-        method: 'GET',
-        params: ''
-      }, options.$http)).success(function (data) {
-        deferred.resolve(data);
-      }).error(function (data) {
-        deferred.reject(options.key);
+      
+      if (!options.files) {
+        options.files = [{
+          prefix: options.prefix,
+          suffix: options.suffix
+        }];
+      }
+      
+      var load = function (file) {
+        if (!file || (!angular.isString(file.prefix) || !angular.isString(file.suffix))) {
+          throw new Error('Couldn\'t load static file: no prefix or suffix specified!');
+        }
+        
+        var deferred = $q.defer();
+        
+        $http(angular.extend({
+          url: [
+            file.prefix,
+            file.key,
+            file.suffix
+          ].join(''),
+          method: 'GET',
+          params: ''
+        }, file.$http)).success(function (data) {
+          deferred.resolve(data);
+        }).error(function (data) {
+          deferred.reject(file.key);
+        });
+        
+        return deferred.promise;
+      };
+      
+      var deferred = $q.defer(),
+          promises = [],
+          length = options.files.length;
+
+      for (var i = 0; i < length; i++) {
+        promises.push(load({
+          prefix: options.files[i].prefix,
+          key: options.key,
+          suffix: options.files[i].suffix
+        }));
+      }
+      
+      $q.all(promises).then(function (data) {
+        var length = data.length,
+            mergedData = {};
+        
+        for (var i = 0; i < length; i++) {
+          angular.forEach(data[i], function(value, key) {
+            mergedData[key] = value;
+          });
+        }
+        
+        deferred.resolve(mergedData);
+      }, function (data) {
+        deferred.reject(data);
       });
+
       return deferred.promise;
     };
   }
